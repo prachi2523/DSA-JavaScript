6b8e6148975ab1b8693cc9f67efed4378ed9650a
prachi verma <prachiverma2521@gmail.com>
2025-07-08 00:12:37 +0530

    added binary questions

---
+++ binary-search/peak-index-in-a-mountain-array.js
@@ -0,0 +1,16 @@
+var peakIndexInMountainArray = function (arr) {
+    let start = 0
+    let end = arr.length - 1
+
+    while (start != end) {
+        let mid = Math.floor((start + end) / 2)
+        if (arr[mid] > arr[mid + 1]) {
+            end = mid
+        } else {
+            start = mid + 1
+        }
+    }
+    return start
+};
+
+console.log(peakIndexInMountainArray([0, 2, 1, 0]))
